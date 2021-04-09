52704125e90f99a945c592e741d8902feb08ed07
Armand <armand.gillot@gmail.com>
2021-04-09 13:28:53 +0200

    first commit

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ router.post('/sign-up', async (req, res, next) => {
 
   if(await usersModel.findOne({email: req.body.email})) {
     console.log('User existe déjà')
-    res.render('../');
+    res.render('https://rainorsun.herokuapp.com/');
   } else {
     var newUsers = new usersModel({
       username: req.body.username,
@@ -38,7 +38,7 @@ router.post('/sign-up', async (req, res, next) => {
     res.redirect('/weather'); 
   
   } else {
-      res.redirect('../');
+      res.redirect('https://rainorsun.herokuapp.com/');
     }
   });
   
@@ -47,7 +47,7 @@ router.post('/sign-up', async (req, res, next) => {
       req.session.user = null
       currentUser = null
       console.log(req.session.user);
-      res.redirect('../');
+      res.redirect('https://rainorsun.herokuapp.com/');
   });
 
 module.exports = router;
